81a1dc09192f66663e1eb0fa6c6ef5b0a4263e85
Helen <helenmperham@gmail.com>
2022-10-23 16:16:10 -0400

    purple photo

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "I'm an artist, and I love the color purple.";
 document.querySelector("body").appendChild(h2)
